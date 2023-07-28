a0e1364c550349e679a821a93d3ba5fd85c23323
jeswinkjohnson <63941715+jeswinkjohnson@users.noreply.github.com>
2023-07-28 19:14:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { readFile } = require("fs")
 const app = express()
 app.use(express.static('Assets'))
 app.get("/", function (req, res) {
-    readFile("./opt/render/project/src/loader.html", "UTF-8", function (err, data) {
+    readFile("/opt/render/project/src/loader.html", "UTF-8", function (err, data) {
         if (err) {
             res.sendStatus("500").send("Out of Order")
         }
